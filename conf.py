a236bb20c7a82aa8a9be7055f0973123c8a834e2
Christoph Buchner <bilderbuchi@phononoia.at>
2023-01-18 21:20:44 +0100

    Document packages, use Sphinx RTD theme.

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@ author = 'PyMeasure maintainers'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser', 'sphinxcontrib.mermaid']
+extensions = ['myst_parser', 'sphinxcontrib.mermaid', 'sphinx_rtd_theme']
 
 myst_enable_extensions = [
   #"colon_fence",
@@ -27,5 +27,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
